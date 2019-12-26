e8dc1e3297cf0b2a7f4f6012a0c2adc348ab329a
lenok97 <iamhappluck@gmail.com>
2019-12-26 19:57:56 +0500

    long long

--- lab1/lab1/lab1.cpp
+++ lab1/lab1/lab1.cpp
@@ -14,6 +14,7 @@ int calculate_term(int k)
 int main(int argc, char** argv)
 {
 	int rank, size, startTag = 0, endTag = 1, exchangeTag = 3;
+	double endTime, startTime = 0.0;
 	MPI_Status status;
 	MPI_Init(&argc, &argv);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
@@ -21,15 +22,16 @@ int main(int argc, char** argv)
 
 	while (true)
 	{
-		double sum=0, temp_sum = 0, endTime, startTime = 0.0;
-		unsigned int n = 0, workPerProc, startPoint, endPoint, nproc;
-		nproc = size;
+		long n = 0, workPerProc, startPoint, endPoint;
+		long long sum = 0, temp_sum = 0;
+		int nproc = size;
 
 		if (rank == root)
 		{
 			cout << "Process count = " << size << endl;
 			cout << "Enter n: ";
 			cin >> n;
+			startTime = MPI_Wtime();
 			workPerProc = n / size;
 			int rest = n % size;
 			int start = 0;
@@ -65,22 +67,24 @@ int main(int argc, char** argv)
 		{
 			if (rank < nproc / 2 )
 			{
-				MPI_Recv(&temp_sum, 1, MPI_DOUBLE,nproc - rank - 1, exchangeTag, MPI_COMM_WORLD, &status);
+				MPI_Recv(&temp_sum, 1, MPI_LONG_LONG_INT,nproc - rank - 1, exchangeTag, MPI_COMM_WORLD, &status);
 				sum += temp_sum;
 			}
 			else 
-				if (rank < nproc  && (nproc%2==0 || ((nproc % 2 == 1) && (rank != nproc / 2))))
-				MPI_Send(&sum, 1, MPI_DOUBLE, nproc - rank - 1, exchangeTag, MPI_COMM_WORLD);
+				if (rank < nproc)  
+					if  (nproc % 2 == 0 || ((nproc % 2 == 1) && (rank != nproc / 2)))
+						MPI_Send(&sum, 1, MPI_LONG_LONG_INT, nproc - rank - 1, exchangeTag, MPI_COMM_WORLD);
+			
 			if (!(nproc % 2 == 0))
 			{
 				if (rank == nproc / 2)
 				{
-					MPI_Send(&sum, 1, MPI_DOUBLE, root, exchangeTag, MPI_COMM_WORLD);
+					MPI_Send(&sum, 1, MPI_LONG_LONG_INT, root, exchangeTag, MPI_COMM_WORLD);
 				}
 
 				if (rank == root)
 				{
-					MPI_Recv(&temp_sum, 1, MPI_DOUBLE, nproc / 2, exchangeTag, MPI_COMM_WORLD, &status);
+					MPI_Recv(&temp_sum, 1, MPI_LONG_LONG_INT, nproc / 2, exchangeTag, MPI_COMM_WORLD, &status);
 					sum+= temp_sum;
 				}
 
@@ -90,7 +94,10 @@ int main(int argc, char** argv)
 
 		if (rank == root)
 		{
-			cout << sum<< endl;
+			endTime = MPI_Wtime();
+			cout <<"Sum: "<< sum<< endl;
+			cout.precision(3);
+			cout << "Time elapsed: " << (endTime - startTime) * 1000 << "ms\n" << endl << endl;
 		}
 	}
 	MPI_Finalize();
